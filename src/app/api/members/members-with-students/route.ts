b5b34aff59a3bd01eac0ad17274a25e20a117cf9
hyunjung <sspp4555@naver.com>
2025-09-28 20:12:40 +0900

    ingyo update

--- src/app/api/members/members-with-students/route.ts
+++ src/app/api/members/members-with-students/route.ts
@@ -17,22 +17,23 @@ export async function GET(req: Request) {
         }
 
         const query = `
-            SELECT
-                m.고유번호,
-                m.이름,
-                m.지역,
-                m.ban,
-                COUNT(s.발_완료일) FILTER (WHERE s.발_완료일 BETWEEN $1 AND $2) AS 발_건수,
-                COUNT(s.찾_완료일) FILTER (WHERE s.찾_완료일 BETWEEN $1 AND $2) AS 찾_건수,
-                COUNT(s.합_완료일) FILTER (WHERE s.합_완료일 BETWEEN $1 AND $2) AS 합_건수,
-                COUNT(s.섭_완료일) FILTER (WHERE s.섭_완료일 BETWEEN $1 AND $2) AS 섭_건수,
-                COUNT(s.복_완료일) FILTER (WHERE s.복_완료일 BETWEEN $1 AND $2) AS 복_건수
-            FROM members m
-            LEFT JOIN students s
-                ON m.고유번호 = s.인도자_고유번호 OR m.고유번호 = s.교사_고유번호
-            WHERE m.ban IS NOT NULL
-            GROUP BY m.고유번호, m.이름, m.지역, m.ban
-            ORDER BY m.지역, m.ban
+        SELECT
+        m.고유번호,
+        m.이름,
+        m.지역,
+        m.ban,
+        COUNT(*) FILTER (WHERE s.발_완료일 BETWEEN $1 AND $2 AND m.고유번호 = s.인도자_고유번호) AS 발_건수,
+        COUNT(*) FILTER (WHERE s.찾_완료일 BETWEEN $1 AND $2 AND m.고유번호 = s.인도자_고유번호) AS 찾_건수,
+        COUNT(*) FILTER (WHERE s.합_완료일 BETWEEN $1 AND $2 AND m.고유번호 = s.인도자_고유번호) AS 합_건수,
+        COUNT(*) FILTER (WHERE s.섭_완료일 BETWEEN $1 AND $2 AND m.고유번호 = s.교사_고유번호) AS 섭_건수,
+        COUNT(*) FILTER (WHERE s.복_완료일 BETWEEN $1 AND $2 AND m.고유번호 = s.교사_고유번호) AS 복_건수
+    FROM members m
+    LEFT JOIN students s
+        ON m.고유번호 = s.인도자_고유번호
+        OR m.고유번호 = s.교사_고유번호
+    WHERE m.ban IS NOT NULL
+    GROUP BY m.고유번호, m.이름, m.지역, m.ban
+    ORDER BY m.지역, m.ban;
         `;
 
         const { rows } = await client.query(query, [startDate, endDate]);
