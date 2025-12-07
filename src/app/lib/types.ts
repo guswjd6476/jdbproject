88c8dab7106246cc5158c9499bf5c803b8487cc3
hyunjung <sspp4555@naver.com>
2025-12-07 23:43:36 +0900

    score update

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -100,12 +100,12 @@ export interface RawStudent {
 export type Region = (typeof REGIONS)[number];
 
 export const DEFAULT_예정_goals: Record<Region, 예정Goals> = {
-    도봉: { team1: '2', team2: '2', team3: '2', team4: '2' },
-    성북: { team1: '2', team2: '2', team3: '2', team4: '2', team5: '2' },
-    노원: { team1: '2', team2: '2', team3: '2', team4: '2' },
-    중랑: { team1: '2', team2: '2', team3: '2', team4: '2' },
-    강북: { team1: '2', team2: '2', team3: '2', team4: '2' },
-    대학: { team1: '3', team2: '3', team3: '3', team4: '3' },
+    도봉: { team1: '1.5', team2: '1.5', team3: '1.5', team4: '1.5' },
+    성북: { team1: '1.5', team2: '1.5', team3: '1.5', team4: '1.5', team5: '1.5' },
+    노원: { team1: '1.5', team2: '1.5', team3: '1.5', team4: '1.5' },
+    중랑: { team1: '1.5', team2: '1.5', team3: '1.5', team4: '1.5' },
+    강북: { team1: '1.5', team2: '1.5', team3: '1.5', team4: '1.5' },
+    대학: { team1: '1.5', team2: '1.5', team3: '1.5', team4: '1.5' },
     새신자: { team1: '0', team2: '0' },
 };
 
